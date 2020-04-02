338c2cf7ab8d1ea321320c080f5fd4bd485490f7
alexis villaraza <alexis.borja@gmail.com>
2020-04-01 17:04:47 -0700

    added PrivateRoute

--- boss-participation/src/App.js
+++ boss-participation/src/App.js
@@ -1,34 +1,33 @@
 import React from "react";
 import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+import PrivateRoute from "./components/PrivateRoute";
 // import Activities from "./components/Activities";
-import Dashboard from './components/Dashboard';
-import ActivityForm from './components/ActivitiyForm';
-import UpdateActivityForm from './components/UpdateActivityForm';
-import Login from './components/Login';
-import Signup from './components/Signup';
+import Dashboard from "./components/Dashboard";
+import ActivityForm from "./components/ActivitiyForm";
+import UpdateActivityForm from "./components/UpdateActivityForm";
+import Login from "./components/Login";
+import Signup from "./components/Signup";
 
 import "./App.css";
 // import axios from "axios";
 
 function App() {
-
   return (
     <>
       <Router>
         <Switch>
           {/* <Route path="/activities" component={Activities} /> */}
-          <Route path="/dashboard" component={Dashboard} />
-          <Route path='/add-activity' component={ActivityForm} />
-          <Route path='/update-activity/:id' component={UpdateActivityForm} />
+          <PrivateRoute path="/dashboard" component={Dashboard} />
+          <PrivateRoute path="/add-activity" component={ActivityForm} />
+          <PrivateRoute
+            path="/update-activity/:id"
+            component={UpdateActivityForm}
+          />
 
-          <Route exact path='/' component={Login} />
+          <Route exact path="/" component={Login} />
           <Route path="/sign-up" component={Signup} />
-      
-          
-
         </Switch>
       </Router>
-  
     </>
   );
 }
